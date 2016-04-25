36d6cae90077ffbe36aff324c830742959a8282b
Tobi80 <t.brunsi@gmx.net>
2016-04-25 21:07:23 +0200

    Fix

--- app/src/main/java/com/tobi/ibsbuch/Splashscreen.java
+++ app/src/main/java/com/tobi/ibsbuch/Splashscreen.java
@@ -6,12 +6,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkRequest;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.Snackbar;
-import android.util.Log;
 import android.view.Window;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
@@ -26,7 +27,8 @@ public class Splashscreen extends Activity {
     private WifiManager wifi;
     private static int SPLASH_TIME_OUT = 3200; //4000
     private WebView webView;
-    private NetworkMonitorReceiver receiver;
+    //    private NetworkMonitorReceiver receiver;
+    ConnectivityManager connMan;
     //  private static final int PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION = 0;
 //  private static final int REQUEST_CODE = 0;
     private Snackbar snackbar, snackbar2;
@@ -61,72 +63,173 @@ public class Splashscreen extends Activity {
         webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
         webView.loadUrl("file:///android_asset/ibs.gif");
 
+
         final WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
         wifi.setWifiEnabled(true);
 
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+    BroadcastReceiver MyBroadcastReceiver = new BroadcastReceiver() {
 
-        this.receiver = new NetworkMonitorReceiver();
-
-        this.registerReceiver(this.receiver, new IntentFilter("android.net.conn.CONNECTIVITY_CHANGE"));
-
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        this.unregisterReceiver(this.receiver);
-    }
-
-    private class NetworkMonitorReceiver extends BroadcastReceiver {
-
-        private final String TAG = NetworkMonitorReceiver.class.getSimpleName();
 
         @Override
         public void onReceive(Context context, Intent intent) {
 
-            ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-            if (networkInfo != null && networkInfo.isConnected()) {
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                final ConnectivityManager manager = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+                final NetworkRequest.Builder builder;
+                builder = new NetworkRequest.Builder();
 
-                if (networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
-                    snackbar.dismiss();
-                    RelativeLayout parentLayout = (RelativeLayout) findViewById(R.id.coordinatorLayout);
-                    snackbar2 = Snackbar
-                            .make(parentLayout, "IBS Wlan verbunden. Starte IBS-Buch", Snackbar.LENGTH_LONG);
+                //set the transport type do WIFI
+                builder.addTransportType(NetworkCapabilities.TRANSPORT_WIFI);
 
-                    snackbar2.show();
-                    new Handler().postDelayed(new Runnable() {
+                manager.requestNetwork(builder.build(), new ConnectivityManager.NetworkCallback() {
 
-                        @Override
-                        public void run() {
+                    @Override
+                    public void onAvailable(Network network) {
 
-                            Intent i = new Intent(Splashscreen.this, MainActivity.class);
-//                            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                            startActivity(i);
-                            finish();
+                        snackbar.dismiss();
+                        RelativeLayout parentLayout = (RelativeLayout) findViewById(R.id.coordinatorLayout);
+                        snackbar2 = Snackbar
+                                .make(parentLayout, "IBS Wlan verbunden. Starte IBS-Buch", Snackbar.LENGTH_LONG);
+                        snackbar2.show();
+                        new Handler().postDelayed(new Runnable() {
+
+                            @Override
+                            public void run() {
+
+                                Intent i = new Intent(Splashscreen.this, MainActivity.class);
+                                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(i);
+                                int pid = android.os.Process.myPid();
+                                android.os.Process.killProcess(pid);
+                                finish();
 
 
+                            }
+                        }, SPLASH_TIME_OUT);
+
+
+                        try {
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
                         }
-                    }, SPLASH_TIME_OUT);
-                }
+                        // Connectet dann nicht!?
+//                        manager.unregisterNetworkCallback(this);
 
+                    }
 
-            } else {
-                Log.d(TAG, "Network is not available");
-
+                });
             }
 
         }
+    };
+
+
+    @Override
+    protected void onResume() {
+        registerReceiver(MyBroadcastReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        try {
+            unregisterReceiver(MyBroadcastReceiver);
+        } catch (IllegalArgumentException e) {
+            if (e.getMessage().contains("Receiver not registered")) {
+
+            } else {
+
+                throw e;
+            }
+        }
+
+        super.onPause();
 
     }
 }
 
+
+//        final WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+//        wifi.setWifiEnabled(true);
+//
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//
+//        this.receiver = new NetworkMonitorReceiver();
+//
+//        this.registerReceiver(this.receiver, new IntentFilter("android.net.conn.CONNECTIVITY_CHANGE"));
+//
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//
+//        this.unregisterReceiver(this.receiver);
+//    }
+//
+//    private class NetworkMonitorReceiver extends BroadcastReceiver {
+//
+//        private final String TAG = NetworkMonitorReceiver.class.getSimpleName();
+//
+//        @Override
+//        public void onReceive(Context context, Intent intent) {
+//
+//            final ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+//            final NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+//
+//            final NetworkRequest.Builder builder = new NetworkRequest.Builder();
+//            builder.addTransportType(NetworkCapabilities.TRANSPORT_WIFI);
+//
+//            connectivityManager.requestNetwork(builder.build(), new ConnectivityManager.NetworkCallback() {
+//
+//                @Override
+//                public void onAvailable(Network network) {
+//
+//            if (networkInfo != null && networkInfo.getType() == ConnectivityManager.TYPE_WIFI && networkInfo.isConnected()) {
+////                        if (networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+//
+//                snackbar.dismiss();
+//                RelativeLayout parentLayout = (RelativeLayout) findViewById(R.id.coordinatorLayout);
+//                snackbar2 = Snackbar
+//                        .make(parentLayout, "IBS Wlan verbunden. Starte IBS-Buch", Snackbar.LENGTH_LONG);
+//                snackbar2.show();
+//
+//                new Handler().postDelayed(new Runnable() {
+//
+//                    @Override
+//                    public void run() {
+//
+//                        Intent i = new Intent(Splashscreen.this, MainActivity.class);
+//
+//                        startActivity(i);
+//                        finish();
+//                        connectivityManager.unregisterNetworkCallback();
+//
+//                    }
+//                }, SPLASH_TIME_OUT);
+//
+//
+//            } else {
+//                Log.d(TAG, "Network is not available");
+//
+//            }
+//
+//
+//        }
+////            }
+//            });
+//        }
+//
+//    }
+//
+//}
 
 
 
